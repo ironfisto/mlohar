5a434c5b466726e77870abb6fe0c15d5b3ed0e44
ironfisto <mukulmalviya2@gmail.com>
2026-03-28 14:44:27 +0530

    added headers

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@ import createMDX from '@next/mdx';
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  async headers() {
+    return [
+      {
+        source: '/:all*.(png|jpg|jpeg|gif|svg|webp|ico)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const withMDX = createMDX({
